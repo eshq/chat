1de563936be5dc7a30f4ade6561bbe0b88b96c25
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2011-09-27 17:15:18 +0200

    Toched up the styles a little

--- public/chat.js
+++ public/chat.js
@@ -1,18 +1,28 @@
 (function($) {
-  var output = $("#messages");
-  var input  = $("#message input");
-
-  eshq.open("chat", {
-    message: function(e) {
-      output.append("<p>" + e.data + "</p>");
-      output[0].scrollTop = output[0].scrollHeight;
-    }
-  });
-
-  $("#message").submit(function(e) {
+  $("#login").submit(function(e) {
     e.preventDefault();
 
-    eshq.send("chat", input.val());
-    input.val("");
+    var nick   = $("#login input").val();
+
+    var output = $("#messages");
+    var input  = $("#message input");
+
+    eshq.open("chat", {
+      open: function(e) {
+        $("#overlay, .login-box").hide();
+      },
+      message: function(e) {
+        var data = JSON.parse(e.data);
+        output.append("<p><strong class='from'>" + data.nick + ":</strong> " + data.msg + "</p>");
+        output[0].scrollTop = output[0].scrollHeight;
+      }
+    });
+
+    $("#message").submit(function(e) {
+      e.preventDefault();
+
+      eshq.send("chat", JSON.stringify({nick: nick, msg: input.val()}));
+      input.val("");
+    });
   });
 })(jQuery);
