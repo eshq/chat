d3d2e03b48e26416a655faefe569ca0f48faa790
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2011-10-17 03:46:10 +0200

    Add ols style debuggging alerts for ie7

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,5 @@
 (function($) {
+  alert("Preparing");
   $("#login").submit(function(e) {
     e.preventDefault();
 
@@ -7,19 +8,23 @@
     var output = $("#messages");
     var input  = $("#message input");
 
+    alert("opening");
     var eshq = new ESHQ("chat", {presence_id: nick});
 
     eshq.onopen = function(e) {
+      alert("open");
       $("#overlay, .login-box").hide();
     };
 
     eshq.onmessage = function(e) {
+      alert("message");
       var data = JSON.parse(e.data);
       output.append("<p><strong class='from'>" + data.nick + ":</strong> " + data.msg + "</p>");
       output[0].scrollTop = output[0].scrollHeight;
     };
 
     $("#message").submit(function(e) {
+      alert("send");
       e.preventDefault();
 
       eshq.send(JSON.stringify({nick: nick, msg: input.val()}));
