9ba4c5278e84aa16c063eb42a931103a8a28978b
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2011-10-17 04:05:59 +0200

    Removed alerts

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,4 @@
 (function($) {
-  alert("Preparing");
   $("#login").submit(function(e) {
     e.preventDefault();
 
@@ -8,23 +7,19 @@
     var output = $("#messages");
     var input  = $("#message input");
 
-    alert("opening");
     var eshq = new ESHQ("chat", {presence_id: nick});
 
     eshq.onopen = function(e) {
-      alert("open");
       $("#overlay, .login-box").hide();
     };
 
     eshq.onmessage = function(e) {
-      alert("message");
       var data = JSON.parse(e.data);
       output.append("<p><strong class='from'>" + data.nick + ":</strong> " + data.msg + "</p>");
       output[0].scrollTop = output[0].scrollHeight;
     };
 
     $("#message").submit(function(e) {
-      alert("send");
       e.preventDefault();
 
       eshq.send(JSON.stringify({nick: nick, msg: input.val()}));
